9c67b61ec5132e3419c4787305dfddc3736cd2e8
Sergey Yelin <elinsn@gmail.com>
2022-01-25 08:30:57 +0300

    JD-776: Fix NPE on optional tradeBlocks field in Country object

--- src/main/java/com/rbkmoney/analytics/listener/handler/dominant/CountryDominantHandler.java
+++ src/main/java/com/rbkmoney/analytics/listener/handler/dominant/CountryDominantHandler.java
@@ -49,9 +49,10 @@ public class CountryDominantHandler extends AbstractDominantHandler {
         country.setCountryId(countryObject.getRef().getId().name());
         com.rbkmoney.damsel.domain.Country data = countryObject.getData();
         country.setName(data.getName());
-        String[] tradeBlocs = data.getTradeBlocs().stream()
+        String[] tradeBlocs = data.isSetTradeBlocs() ? data.getTradeBlocs()
+                .stream()
                 .map(TradeBlocRef::getId)
-                .toArray(String[]::new);
+                .toArray(String[]::new) : null;
         country.setTradeBloc(tradeBlocs);
         return country;
     }
