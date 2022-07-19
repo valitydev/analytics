63772553caa55b7fd8bed7717823f2dbd6d8bf76
Egor Cherniak <cherniak3@yandex.ru>
2022-07-19 16:26:14 +0300

    new kafka config

--- src/main/java/dev/vality/analytics/config/KafkaConfig.java
+++ src/main/java/dev/vality/analytics/config/KafkaConfig.java
@@ -1,11 +1,9 @@
 package dev.vality.analytics.config;
 
-import dev.vality.analytics.config.properties.KafkaSslProperties;
 import dev.vality.analytics.serde.MachineEventDeserializer;
 import dev.vality.analytics.serde.PayoutEventDeserializer;
 import dev.vality.machinegun.eventsink.MachineEvent;
 import dev.vality.mg.event.sink.service.ConsumerGroupIdService;
-import dev.vality.mg.event.sink.utils.SslKafkaUtils;
 import dev.vality.payout.manager.Event;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +12,7 @@ import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -22,7 +20,6 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.SeekToCurrentBatchErrorHandler;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.kafka.clients.consumer.OffsetResetStrategy.EARLIEST;
@@ -30,41 +27,24 @@ import static org.apache.kafka.clients.consumer.OffsetResetStrategy.EARLIEST;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-@EnableConfigurationProperties(KafkaSslProperties.class)
 public class KafkaConfig {
 
     public static final String ANALYTICS_RATE = "rate";
     private static final String RESULT_ANALYTICS = "result-analytics";
     private static final String PARTY_RESULT_ANALYTICS = "party-result-analytics";
     private final ConsumerGroupIdService consumerGroupIdService;
-    private final KafkaSslProperties kafkaSslProperties;
+    private final KafkaProperties kafkaProperties;
+
     @Value("${kafka.max.poll.records}")
     private String maxPollRecords;
     @Value("${kafka.topic.party.max.poll.records}")
     private String maxPollRecordsPartyListener;
     @Value("${kafka.topic.rate.max.poll.records}")
     private String maxPollRecordsRatesListener;
-    @Value("${kafka.max.poll.interval.ms}")
-    private int maxPollInterval;
-    @Value("${kafka.max.session.timeout.ms}")
-    private int sessionTimeout;
-    @Value("${kafka.bootstrap.servers}")
-    private String bootstrapServers;
     @Value("${kafka.consumer.concurrency}")
     private int concurrencyListeners;
     @Value("${kafka.topic.rate.groupId}")
     private String rateGroupId;
-
-    private Map<String, Object> createSslConfig() {
-        log.info("Kafka ssl isEnabled: {}", kafkaSslProperties.isEnabled());
-        return SslKafkaUtils.sslConfigure(
-                kafkaSslProperties.isEnabled(),
-                kafkaSslProperties.getTrustStoreLocation(),
-                kafkaSslProperties.getTrustStorePassword(),
-                kafkaSslProperties.getKeyStoreLocation(),
-                kafkaSslProperties.getKeyStorePassword(),
-                kafkaSslProperties.getKeyPassword());
-    }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, MachineEvent> invoiceListenerContainerFactory() {
@@ -121,7 +101,7 @@ public class KafkaConfig {
     private <T> DefaultKafkaConsumerFactory<String, T> createKafkaConsumerFactory(String consumerGroup,
                                                                                   Deserializer<T> deserializer,
                                                                                   String maxPollRecords) {
-        final Map<String, Object> props = createDefaultProperties(consumerGroup);
+        final Map<String, Object> props = createDefaultConsumerProperties(consumerGroup);
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPollRecords);
         return new DefaultKafkaConsumerFactory<>(
                 props,
@@ -129,16 +109,11 @@ public class KafkaConfig {
                 deserializer);
     }
 
-    private Map<String, Object> createDefaultProperties(String value) {
-        final Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    private Map<String, Object> createDefaultConsumerProperties(String value) {
+        final Map<String, Object> props = kafkaProperties.buildConsumerProperties();
         props.put(ConsumerConfig.GROUP_ID_CONFIG, value);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, EARLIEST.name().toLowerCase());
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, sessionTimeout);
-        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, maxPollInterval);
-        props.putAll(createSslConfig());
-
         return props;
     }
 }
