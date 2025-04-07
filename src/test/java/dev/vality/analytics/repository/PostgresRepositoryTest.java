478e0c2d340bf937fdb952ce5eac2deec6223f2a
malkoas <alien.life.form.1986@gmail.com>
2025-04-07 16:12:59 +0300

    BG-55: bump flyway

--- src/test/java/dev/vality/analytics/repository/PostgresRepositoryTest.java
+++ src/test/java/dev/vality/analytics/repository/PostgresRepositoryTest.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
