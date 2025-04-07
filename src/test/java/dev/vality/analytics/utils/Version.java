0c3580a3f8006252a28efe59cb1497e11544aaa2
malkoas <alien.life.form.1986@gmail.com>
2025-04-07 13:52:08 +0300

    BG-55: bump flyway

--- src/test/java/dev/vality/analytics/utils/Version.java
+++ src/test/java/dev/vality/analytics/utils/Version.java
@@ -2,6 +2,6 @@ package dev.vality.analytics.utils;
 
 public class Version {
 
-    public static final String POSTGRES_VERSION = "postgres:13.5";
+    public static final String POSTGRES_VERSION = "postgres:17.4";
 
 }
